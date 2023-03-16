397841ead594bfcc35384e76d11cf29f40334855
Cristiano  Gomes <cristiano1981gomes@gmail.com>
2023-03-17 00:21:12 +0044

    Aula de escopos criando mini insta

---
+++ src/controladores/postagens.js
@@ -0,0 +1,155 @@
+const knex = require("../conexao");
+
+const novaPostagem = async (req, res) => {
+  const { id } = req.usuario;
+  const { texto, fotos } = req.body;
+
+  if (!fotos || !fotos.length) {
+    return res.status(404).json("É preciso informar pelo menos uma foto");
+  }
+
+  try {
+    const postagem = await knex("postagens")
+      .insert({ texto, usuario_id: id })
+      .returning("*");
+
+    if (!postagem) {
+      return res.status(400).json("Não foi possível cadastrar a postagem");
+    }
+
+    for (const foto of fotos) {
+      foto.postagem_id = postagem[0].id;
+    }
+
+    const fotosCadastradas = await knex("postagem_fotos").insert(fotos);
+
+    if (!fotosCadastradas) {
+      await knex("postagens").where({ id: postagem[0].id }).delete();
+      return res.status(400).json("Não foi possível cadastrar a postagem");
+    }
+
+    return res.status(200).json("Postagem cadastrada com sucesso");
+  } catch (error) {
+    console.log({ mensagem: "é aqui" });
+    return res.status(400).json(error.message);
+  }
+};
+
+const curtir = async (req, res) => {
+  const { id } = req.usuario;
+  const { postagemId } = req.params;
+
+  try {
+    const postagem = await knex("postagens").where({ id: postagemId }).first();
+
+    if (!postagem) {
+      return res.status(404).json("Postagem não encontrada");
+    }
+
+    const jaCurtiu = await knex("postagem_curtidas")
+      .where({ usuario_id: id, postagem_id: postagem.id })
+      .first();
+
+    if (jaCurtiu) {
+      return res.status(404).json("Você já curtiu essa postagem");
+    }
+
+    const curtida = await knex("postagem_curtidas")
+      .insert({ usuario_id: id, postagem_id: postagem.id })
+      .returning("*");
+
+    if (!curtida) {
+      return res.status(400).json("Não foi possível curtir a postagem");
+    }
+    return res.status(200).json("Postagem curtida com sucesso");
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const comentar = async (req, res) => {
+  const { id } = req.usuario;
+  const { postagemId } = req.params;
+  const { texto } = req.body;
+
+  if (!texto) {
+    return res.status(404).json("O campo texto é obrigatório");
+  }
+  try {
+    const postagem = await knex("postagens").where({ id: postagemId }).first();
+
+    if (!postagem) {
+      return res.status(404).json("Postagem não encontrada");
+    }
+
+    const comentario = await knex("postagem_comentarios")
+      .insert({ usuario_id: id, postagem_id: postagem.id, texto })
+      .returning("*");
+
+    if (!comentario) {
+      return res.status(400).json("Não foi possível comentar a postagem");
+    }
+    return res.status(200).json("Postagem comentada com sucesso");
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const feed = async (req, res) => {
+  const { id } = req.usuario;
+  const { offset } = req.query;
+
+  const limite = offset ? offset : 0;
+  try {
+    const postagens = await knex("postagens")
+      .where("usuario_id", "!=", id)
+      .limit(10)
+      .offset(limite);
+
+    if (postagens.length === 0) {
+      return res.status(200).json(postagens);
+    }
+
+    for (const postagem of postagens) {
+      //usuario
+      const usuario = await knex("usuarios")
+        .where({ id: postagem.usuario_id })
+        .select("imagem", "username", "verificado")
+        .first();
+      postagem.usuario = usuario;
+
+      //fotos
+      const fotos = await knex("postagem_fotos")
+        .where({ postagem_id: postagem.id })
+        .select("imagem");
+      postagem.fotos = fotos;
+      //curtidas
+      const curtidas = await knex("postagem_curtidas")
+        .where({ postagem_id: postagem.id })
+        .select("usuario_id");
+      postagem.curtidas = curtidas.length;
+
+      //curtidas por mim
+        postagem.curtidasPorMim = curtidas.find(curtida => curtida.usuario_id === id) ? true : false;
+
+      //comentarios
+
+        const comentarios = await knex("postagem_comentarios")
+        .leftJoin("usuarios", "usuarios.id", "postagem_comentarios.usuario_id" )
+        .where({ postagem_id: postagem.id })
+        .select("usuarios.username", "postagem_comentarios.texto");
+        postagem.comentarios = comentarios;
+
+    return res.status(200).json(postagens)
+    }
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+module.exports = {
+  novaPostagem,
+  curtir,
+  comentar,
+  feed,
+};
